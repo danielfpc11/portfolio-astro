10867e0716021e520dfbe0a05eaf176a77065978
Daniel Pulgarin <danielfpc11@gmail.com>
2026-03-25 19:49:32 +0000

    Project setup

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://danielpulgarin.com',
+  integrations: [mdx(), sitemap()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
